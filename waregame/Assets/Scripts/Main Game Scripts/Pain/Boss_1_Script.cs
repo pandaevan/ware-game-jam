d77580531595d0d40b6c85d56bcbf9acd5569c9e
NomMilk <thehuiq2006@gmail.com>
2023-06-10 18:52:32 -0700

    adfjl;ksI hATE PEOEPEL I AHTE EFJAIEOJEFAPJEFJIOEFIJAEF THEIS IS THRE EASON WHY GAY PEOPEL ESIXXTS

---
+++ waregame/Assets/Scripts/Main Game Scripts/Pain/Boss_1_Script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_1_Script : MonoBehaviour
+{
+    public bool Finished;
+
+public void boss1ani()
+{
+    Finished = true;
+}
+}
---
+++ waregame/Assets/Scripts/Main Game Scripts/Pain/Boss_2_Script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_2_Script : MonoBehaviour
+{
+    public  bool Finished;
+public void boss1ani()
+{
+    Finished = true;
+}
+}
